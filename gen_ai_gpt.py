8d7e4a58a83e499982ee0eb98dc21e5f3be98509
Tae wook <laputa99999@gmail.com>
2023-04-09 16:52:27 +0900

    Add files via upload

--- gen_ai_gpt.py
+++ gen_ai_gpt.py
@@ -1,93 +1,97 @@
+# title: Gen AI with GPT and Dall-E2
 # programmer: taewook kang
 # date: 2023.3.30
 # reference
 #   https://openai.com/pricing
 #   https://platform.openai.com
-#   https://developers.kakao.com
-import sys, os, re, argparse, readline, json, glob, shutil
+#   https://platform.openai.com/account/usage
+#
+import sys, os, re, argparse, json
 from yaml import parse
 
 import openai
-OPENAI_API_KEY = "" # ADD OPENAI API KEY
+OPENAI_API_KEY = "" # TODO. ADD OPENAI API KEY
 
 def gen_ai_prompt(query):
-    # create text using chatgpt
-    # https://platform.openai.com/account/usage
+    try:
+        # description in detail from query 
+        openai.api_key = OPENAI_API_KEY
 
-    # prompt 1
-    openai.api_key = OPENAI_API_KEY
+        model = "gpt-3.5-turbo"
+        messages = [
+            {
+                "role": "system",
+                "content": "You are a helpful assistant who is good at detailing."
+            },
+            {
+                "role": "user",
+                "content": query
+            }
+        ]
 
-    model = "gpt-3.5-turbo"
-    messages = [
-        {
-            "role": "system",
-            "content": "You are a helpful assistant who is good at detailing."
-        },
-        {
-            "role": "user",
-            "content": query
-        }
-    ]
+        response = openai.ChatCompletion.create(
+            model=model,
+            messages=messages
+        )
+        desc = response['choices'][0]['message']['content']
+        print(desc)
 
-    response = openai.ChatCompletion.create(
-        model=model,
-        messages=messages
-    )
-    answer = response['choices'][0]['message']['content']
-    print(answer)
+        # summary for AI image generation
+        messages.append(
+            {
+                "role": "assistant",
+                "content": desc
+            },
+        )
 
-    # prompt 2
-    messages.append(
-        {
-            "role": "assistant",
-            "content": answer
-        },
-    )
+        messages.append(
+            {
+                "role": "user", 
+                "content": "위 내용을 바탕으로 글을 한문장으로 요약해줘."
+            }
+        )
 
-    messages.append(
-        {
-            "role": "user", 
-            "content": "위 내용을 바탕으로 글을 한문장으로 요약해줘."
-        }
-    )
+        response = openai.ChatCompletion.create(
+            model=model,
+            messages=messages
+        )
+        summary = response['choices'][0]['message']['content']
+        print(summary)
 
-    response = openai.ChatCompletion.create(
-        model=model,
-        messages=messages
-    )
-    answer2 = response['choices'][0]['message']['content']
-    print(answer2)
+        # english translation
+        messages = [
+            {
+                "role": "system",
+                "content": "You are a helpful assistant who is good at translating."
+            },
+            {
+                "role": "assistant",
+                "content": summary
+            }
+        ]
 
-    # prompt 3
-    messages = [
-        {
-            "role": "system",
-            "content": "You are a helpful assistant who is good at translating."
-        },
-        {
-            "role": "assistant",
-            "content": answer2
-        }
-    ]
+        messages.append(
+            {
+                "role": "user", 
+                "content": "영어로 번역해줘."
+            }
+        )
 
-    messages.append(
-        {
-            "role": "user", 
-            "content": "영어로 번역해주세요."
-        }
-    )
+        response = openai.ChatCompletion.create(
+            model=model,
+            messages=messages
+        )
+        eng = response['choices'][0]['message']['content']
+        print(eng)
 
-    response = openai.ChatCompletion.create(
-        model=model,
-        messages=messages
-    )
-    answer3 = response['choices'][0]['message']['content']
-    print(answer3)
+        # eng = gen_image_description(model, eng)
+        return eng
+    except Exception as e:
+        print(e)
+    return ""
 
-    return answer3
-
-    '''
-    # prompt 4
+def gen_image_description(model, prompt):
+    # image creation prompt
     messages = [
         {
             "role": "system",
@@ -95,7 +99,7 @@ def gen_ai_prompt(query):
         },
         {
             "role": "assistant",
-            "content": answer3
+            "content": prompt
         }
     ]
     messages.append(
@@ -109,13 +113,12 @@ def gen_ai_prompt(query):
         model=model,
         messages=messages
     )
-    answer4 = response['choices'][0]['message']['content']
-    print(answer4)
-    return answer4
-    '''
+    answer = response['choices'][0]['message']['content']
+    print(answer)
+    return answer
 
 # create image using Dall-E
-def create_ai_image(input, style, fname):
+def create_ai_image(input, style, image_size, fname):
     import requests
     from PIL import Image
     from io import BytesIO
@@ -124,7 +127,7 @@ def create_ai_image(input, style, fname):
     response = openai.Image.create(
             prompt=input,
             n=1,
-            size="512x512"
+            size=image_size
         )
     image_url = response['data'][0]['url']
     res = requests.get(image_url)
@@ -138,11 +141,12 @@ def main():
         parser.add_argument('--query', type=str, default="달의 위를 두발로 걷고 있는 우주복 입은 치즈태비 고양이를 묘사해줘.", required=False)
         parser.add_argument('--style', type=str, default='realistic lighting, ultra-detailed, 8K, photorealism', required=False)
         parser.add_argument('--output', type=str, default='./image.png', required=False)
+        parser.add_argument('--image_size', type=str, default="512x512", required=False)
         args = parser.parse_args()
 
         query = args.query
         prompt = gen_ai_prompt(query)
-        create_ai_image(prompt, args.style, args.output)
+        create_ai_image(prompt, args.style, args.image_size, args.output)
             
     except Exception as e:
         print(e)
